3213eb3f230a7452653cfa1f28fbacba11664c62
v0lt <mpc-be@mail.ru>
2023-03-21 21:52:45 +0300

    Removed native codec for Cineform HD (external dependencies).

--- cineform/build32.bat
+++
@@ -1,8 +0,0 @@
-@set msys=E:\work\ffmpeg_build\msys64
-@set bin=%msys%\mingw32\bin
-@path %path%;%bin%;%msys%\usr\bin
-@set lib=%msys%\..\local32\lib
-@set include=%msys%\..\local32\include
-@set out=obj32
-
-@call build
