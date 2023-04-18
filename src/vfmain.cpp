c181118cea3599fe446bd621065b537305389eb2
v0lt <mpc-be@mail.ru>
2023-04-18 19:27:23 +0300

    Форматирование кода fflayer.

--- src/vfmain.cpp
+++ src/vfmain.cpp
@@ -2,27 +2,27 @@
 #include "fflayer.h"
 
 VDX_DECLARE_VIDEOFILTERS_BEGIN()
-  VDX_DECLARE_VIDEOFILTER(filterDef_fflayer)
+VDX_DECLARE_VIDEOFILTER(filterDef_fflayer)
 VDX_DECLARE_VIDEOFILTERS_END()
 
-extern "C" int __cdecl VirtualdubFilterModuleInit2(VDXFilterModule *fm, const VDXFilterFunctions *ff, int& vdfd_ver, int& vdfd_compat) {
-  if(VDXVideoFilterModuleInit2(fm,ff,vdfd_ver)) return 1;
+extern "C" int __cdecl VirtualdubFilterModuleInit2(VDXFilterModule * fm, const VDXFilterFunctions * ff, int& vdfd_ver, int& vdfd_compat) {
+	if (VDXVideoFilterModuleInit2(fm, ff, vdfd_ver)) return 1;
 
-  vdfd_ver = VIRTUALDUB_FILTERDEF_VERSION;
-  vdfd_compat = 9;		// copy constructor support required
-  return 0;
+	vdfd_ver = VIRTUALDUB_FILTERDEF_VERSION;
+	vdfd_compat = 9;      // copy constructor support required
+	return 0;
 }
 
-extern "C" int __cdecl FilterModModuleInit(VDXFilterModule *fm, const FilterModInitFunctions *ff, int& vdfd_ver, int& vdfd_compat, int& mod_ver, int& mod_min) {
-  if(VDXVideoFilterModuleInitFilterMod(fm,ff,vdfd_ver,mod_ver)) return 1;
+extern "C" int __cdecl FilterModModuleInit(VDXFilterModule * fm, const FilterModInitFunctions * ff, int& vdfd_ver, int& vdfd_compat, int& mod_ver, int& mod_min) {
+	if (VDXVideoFilterModuleInitFilterMod(fm, ff, vdfd_ver, mod_ver)) return 1;
 
-  vdfd_ver = VIRTUALDUB_FILTERDEF_VERSION;
-  vdfd_compat = 9;		// copy constructor support required
+	vdfd_ver = VIRTUALDUB_FILTERDEF_VERSION;
+	vdfd_compat = 9;      // copy constructor support required
 
-  mod_ver = FILTERMOD_VERSION;
-  mod_min = FILTERMOD_VERSION;
-  return 0;
+	mod_ver = FILTERMOD_VERSION;
+	mod_min = FILTERMOD_VERSION;
+	return 0;
 }
 
-extern "C" void __cdecl VirtualdubFilterModuleDeinit(VDXFilterModule *fm, const VDXFilterFunctions *ff) {
+extern "C" void __cdecl VirtualdubFilterModuleDeinit(VDXFilterModule * fm, const VDXFilterFunctions * ff) {
 }
