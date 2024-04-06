532b1e5e2e230e09192ed8f7c21810e9b0b610e8
v0lt <mpc-be@mail.ru>
2024-04-06 21:26:05 +0300

    Добавление к commit af9c606.

---
+++ src/Version.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "../revision.h"
+
+#ifndef REV_DATE
+#define REV_DATE       0
+#endif
+
+#ifndef REV_HASH
+#define REV_HASH       0
+#endif
+
+#ifndef REV_NUM
+#define REV_NUM        0
+#endif
+
+#ifndef REV_BRANCH
+#define REV_BRANCH     LOCAL
+#endif
+
+#define DO_MAKE_STR(x) #x
+#define MAKE_STR(x)    DO_MAKE_STR(x)
+
+#define VER_RELEASE    0
+
+#define VER_MAJOR      0
+#define VER_MINOR      6
+#define VER_BUILD      1
+
+#define VERSION_NUM    VER_MAJOR,VER_MINOR,VER_BUILD,REV_NUM
+#define VERSION_STR    MAKE_STR(VER_MAJOR) "." MAKE_STR(VER_MINOR) "." MAKE_STR(VER_BUILD) "." MAKE_STR(REV_NUM)
+
+#define BRANCH_STR     MAKE_STR(REV_BRANCH)
