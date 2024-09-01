4ed90901330922d54b58f8ad81d623ef92a9d9d2
v0lt <mpc-be@mail.ru>
2024-09-01 18:37:40 +0300

    Удалены неиспользуемые данные.

--- src/gopro.h
+++ src/gopro.h
@@ -5,32 +5,9 @@
 #include <string>
 
 struct GoproInfo {
-	enum CameraEnum {
-		NONE = 0,
-		HERO6_Black,
-		HERO5_Session,
-		HERO5_Black,
-		HERO4_Session,
-		HERO4_Black,
-		HERO4_Silver,
-		HERO3P_Black,
-		HERO3P_Silver,
-		HERO3_Black,
-		HERO3_Silver,
-		HERO3_White,
-		HERO,
-		HEROP,
-		HEROP_LCD,
-		HERO9,
-		HERO10,
-		HERO12,
-	};
-
 	struct CameraType {
 		const char Compare[7];
-		CameraEnum type;
 		const char Name[25];
-		const char ExifModel[25];
 	};
 
 	const CameraType* type = nullptr;
