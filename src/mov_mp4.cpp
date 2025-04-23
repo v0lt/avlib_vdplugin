bc5c742fc5018e58a3739e50319bbffbf8ced986
v0lt <mpc-be@mail.ru>
2025-04-23 06:56:54 +0300

    Косметика для MovParser.

--- src/mov_mp4.cpp
+++ src/mov_mp4.cpp
@@ -7,6 +7,74 @@
 
 #include "mov_mp4.h"
 
+MovParser::MovParser(const void* _buf, int _buf_size, int64_t _fileSize)
+	: buf(_buf)
+	, buf_size(_buf_size)
+	, fileSize(_fileSize)
+	, p((const char*)_buf)
+{
+}
+
+MovParser::MovParser(const std::wstring& name)
+{
+	hfile = CreateFileW(name.c_str(), GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
+	LARGE_INTEGER s;
+	GetFileSizeEx(hfile, &s);
+	fileSize = s.QuadPart;
+}
+
+MovParser::~MovParser()
+{
+	if (hfile) {
+		CloseHandle(hfile);
+	}
+}
+
+bool MovParser::can_read(int64_t s)
+{
+	if (hfile) {
+		if (fileSize - offset < 8) return false;
+	}
+	else {
+		if (buf_size - offset < 8) return false;
+	}
+	return true;
+}
+
+bool MovParser::can_read(MovAtom& a)
+{
+	return offset < a.pos + a.sz;
+}
+
+void MovParser::skip(MovAtom& a)
+{
+	int64_t d = a.pos + a.sz - offset;
+	offset += d;
+	if (hfile) {
+		LARGE_INTEGER distance;
+		distance.QuadPart = offset;
+		SetFilePointerEx(hfile, distance, 0, FILE_BEGIN);
+	}
+	else {
+		p += d;
+	}
+}
+
+void MovParser::read(MovAtom& a, std::unique_ptr<char[]>& buf, int& size, const int extra /*= 0*/)
+{
+	size = int(a.sz - a.hsize);
+	buf.reset(new char[size + extra]);
+	memset(buf.get() + size, 0, extra);
+	if (hfile) {
+		DWORD w;
+		ReadFile(hfile, buf.get(), size, &w, 0);
+	}
+	else {
+		memcpy(buf.get(), p, size); p += size;
+	}
+	offset += size;
+}
+
 uint32_t MovParser::read4()
 {
 	uint32_t r;
@@ -39,7 +107,9 @@ int64_t MovParser::read8()
 
 bool MovParser::read(MovAtom& a)
 {
-	if (!can_read(8)) return false;
+	if (!can_read(8)) {
+		return false;
+	}
 	a.pos = offset;
 	a.sz = read4();
 	a.t = read4();
@@ -49,9 +119,13 @@ bool MovParser::read(MovAtom& a)
 		a.sz = fileSize;
 	}
 	else if (a.sz == 1) {
-		if (!can_read(8)) return false;
+		if (!can_read(8)) {
+			return false;
+		}
 		a.sz = read8();
-		if (a.sz < 16) return false;
+		if (a.sz < 16) {
+			return false;
+		}
 		a.hsize = 16;
 	}
 	else if (a.sz < 8) {
--- src/mov_mp4.h
+++ src/mov_mp4.h
@@ -21,81 +21,24 @@ struct MovAtom {
 };
 
 struct MovParser {
-	const void* buf;
-	int buf_size;
+	const void* buf  = nullptr;
+	int     buf_size = 0;
 
-	const char* p;
-	int64_t offset;
-	int64_t fileSize;
-	HANDLE hfile;
+	const   char* p  = nullptr;
+	int64_t offset   = 0;
+	int64_t fileSize = 0;
+	HANDLE  hfile    = NULL;
 
-	MovParser(const void* _buf, int _buf_size, int64_t _fileSize)
-		: buf(_buf)
-		, buf_size(_buf_size)
-		, fileSize(_fileSize)
-		, p((const char*)_buf)
-		, offset(0)
-		, hfile(nullptr)
-	{
-	}
+	MovParser(const void* _buf, int _buf_size, int64_t _fileSize);
+	MovParser(const std::wstring& name);
+	~MovParser();
 
-	MovParser(const std::wstring& name)
-		: buf(nullptr)
-		, buf_size(0)
-		, p(nullptr)
-		, offset(0)
-	{
-		hfile = CreateFileW(name.c_str(), GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
-		LARGE_INTEGER s;
-		GetFileSizeEx(hfile, &s);
-		fileSize = s.QuadPart;
-	}
+	bool can_read(int64_t s);
+	bool can_read(MovAtom& a);
 
-	~MovParser() {
-		if (hfile) CloseHandle(hfile);
-	}
+	void skip(MovAtom& a);
 
-	bool can_read(int64_t s) {
-		if (hfile) {
-			if (fileSize - offset < 8) return false;
-		}
-		else {
-			if (buf_size - offset < 8) return false;
-		}
-		return true;
-	}
-
-	bool can_read(MovAtom& a) {
-		return offset < a.pos + a.sz;
-	}
-
-	void read(MovAtom& a, std::unique_ptr<char[]>& buf, int& size, const int extra = 0) {
-		size = int(a.sz - a.hsize);
-		buf.reset(new char[size + extra]);
-		memset(buf.get() + size, 0, extra);
-		if (hfile) {
-			DWORD w;
-			ReadFile(hfile, buf.get(), size, &w, 0);
-		}
-		else {
-			memcpy(buf.get(), p, size); p += size;
-		}
-		offset += size;
-	}
-
-	void skip(MovAtom& a) {
-		int64_t d = a.pos + a.sz - offset;
-		offset += d;
-		if (hfile) {
-			LARGE_INTEGER distance;
-			distance.QuadPart = offset;
-			SetFilePointerEx(hfile, distance, 0, FILE_BEGIN);
-		}
-		else {
-			p += d;
-		}
-	}
-
+	void read(MovAtom& a, std::unique_ptr<char[]>& buf, int& size, const int extra = 0);
 	uint32_t read4();
 	int64_t read8();
 	bool read(MovAtom& a);
