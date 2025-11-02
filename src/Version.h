e2b7fbc76e8b1f89e974eca8dc2abbeaedc1a665
v0lt <mpc-be@mail.ru>
2025-11-02 12:18:46 +0300

    Смена версии на 1.1.0.x dev.

--- src/Version.h
+++ src/Version.h
@@ -29,8 +29,8 @@
 #define MAKE_STR(x)    DO_MAKE_STR(x)
 
 #define VER_MAJOR      1
-#define VER_MINOR      0
-#define VER_BUILD      4
+#define VER_MINOR      1
+#define VER_BUILD      0
 
 #define VERSION_NUM    VER_MAJOR,VER_MINOR,VER_BUILD,REV_NUM
 #define VERSION_STR    MAKE_STR(VER_MAJOR) "." MAKE_STR(VER_MINOR) "." MAKE_STR(VER_BUILD) "." MAKE_STR(REV_NUM)
