f514255780e13d9b5304f67c59af246d1174d6c8
v0lt <mpc-be@mail.ru>
2025-11-28 20:39:24 +0300

    Синхронизация файлов VirtualDub2.

--- vd2/h/vd2/VDXFrame/VideoFilter.h
+++ vd2/h/vd2/VDXFrame/VideoFilter.h
@@ -1,26 +1,11 @@
-//	VDXFrame - Helper library for VirtualDub plugins
-//	Copyright (C) 2008 Avery Lee
+// VDXFrame - Helper library for VirtualDub plugins
 //
-//	The plugin headers in the VirtualDub plugin SDK are licensed differently
-//	differently than VirtualDub and the Plugin SDK themselves.  This
-//	particular file is thus licensed as follows (the "zlib" license):
+// Copyright (C) 2008 Avery Lee
+// Copyright (C) 2016-2018 Anton Shekhovtsov
+// Copyright (C) 2024 v0lt
 //
-//	This software is provided 'as-is', without any express or implied
-//	warranty.  In no event will the authors be held liable for any
-//	damages arising from the use of this software.
+// SPDX-License-Identifier: Zlib
 //
-//	Permission is granted to anyone to use this software for any purpose,
-//	including commercial applications, and to alter it and redistribute it
-//	freely, subject to the following restrictions:
-//
-//	1.	The origin of this software must not be misrepresented; you must
-//		not claim that you wrote the original software. If you use this
-//		software in a product, an acknowledgment in the product
-//		documentation would be appreciated but is not required.
-//	2.	Altered source versions must be plainly marked as such, and must
-//		not be misrepresented as being the original software.
-//	3.	This notice may not be removed or altered from any source
-//		distribution.
 
 #ifndef f_VD2_VDXFRAME_VIDEOFILTER_H
 #define f_VD2_VDXFRAME_VIDEOFILTER_H
--- vd2/h/vd2/plugin/vdvideofilt.h
+++ vd2/h/vd2/plugin/vdvideofilt.h
@@ -1,19 +1,11 @@
-//	VirtualDub - Video processing and capture application
-//	Copyright (C) 1998-2001 Avery Lee
+// VirtualDub - Video processing and capture application
 //
-//	This program is free software; you can redistribute it and/or modify
-//	it under the terms of the GNU General Public License as published by
-//	the Free Software Foundation; either version 2 of the License, or
-//	(at your option) any later version.
+// Copyright (C) 1998-2001 Avery Lee
+// Copyright (C) 2015-2018 Anton Shekhovtsov
+// Copyright (C) 2025 v0lt
 //
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details.
+// SPDX-License-Identifier: GPL-2.0-or-later
 //
-//	You should have received a copy of the GNU General Public License
-//	along with this program; if not, write to the Free Software
-//	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #ifndef f_VD2_PLUGIN_VDVIDEOFILT_H
 #define f_VD2_PLUGIN_VDVIDEOFILT_H
